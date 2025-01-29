ec711991129342fe556f2231709aa54642a2f26e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-28 21:52:21 -0500

    Update AddStudent.js

--- reduxdemo/src/components/AddStudent.js
+++ reduxdemo/src/components/AddStudent.js
@@ -1,6 +1,17 @@
 function AddStudent(){
     return(
        <>
+       <form>
+        <div>
+            RollNo:<input type="text" ></input>
+        </div>
+        <div>
+           Name:<input type="text"></input> 
+        </div>
+        <div>
+            Marks:<input type="text"></input>
+        </div>
+       </form>
        </>
 
     );
