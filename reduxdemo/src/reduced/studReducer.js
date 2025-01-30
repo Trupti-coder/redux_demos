d984f0df8ffaa502cad12fea2e2e4ff07b138e7c
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-29 22:10:26 -0500

    Update studReducer.js

--- reduxdemo/src/reduced/studReducer.js
+++ reduxdemo/src/reduced/studReducer.js
@@ -1,3 +1,14 @@
+const initArr={
+    students:[
+        {
+            rollno:1,name:"aaa",marks:45
+        },
+        {
+            rollno:2,name:"bbb",marks:54
+        }
+    ]
+}
+
 function studReducer(){
     return(
         <>
