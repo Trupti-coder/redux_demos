99134081f4520b624497a0274cabd65a6b704be5
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-30 13:12:37 -0500

    Update store.js

--- reduxdemo/src/store/store.js
+++ reduxdemo/src/store/store.js
@@ -1,2 +1,7 @@
+
+import { createStore } from "redux";
+import studReducer from "../reducer/studReducer";
+import { createStoreHook } from "react-redux";
+
 const store=createStore(studReducer);
 export default store;
